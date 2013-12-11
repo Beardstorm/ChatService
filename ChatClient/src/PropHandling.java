8dd43b43f94dbbe9911a3bbc6745abcccdd74cf7
Mohanad Yousif <Samoyl3000@gmail.com>
2013-12-11 14:34:18 +0100

    Uppdate PropHandling
    This os the uppdating of PropHandling with all comments that i made in
    it.

--- ChatClient/src/PropHandling.java
+++ ChatClient/src/PropHandling.java
@@ -5,29 +5,37 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropHandling {
-
+	
+	//Great a properties object
 	private Properties prop;
 
+	//Great a writing method for at save the port number and localhost
 	public void writerProperties(String fileName) {
 		prop = new Properties();
 		try {
+			//set the properties value
 			prop.setProperty("datorname", fileName);
 			prop.setProperty("port", fileName);
+			
+			//save properties to project root folder
 			prop.store(new FileOutputStream(fileName), null);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	//Great a reading method for at collect port number and localhost
 	public void readProperties(String fileName) {
 		prop = new Properties();
 		try {
+			//collect properties from project root folder
 			prop.load(new FileInputStream(fileName));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	// Great String method for property
 	public String getProperty(String propName) 
 	{
 		return prop.getProperty(propName);
