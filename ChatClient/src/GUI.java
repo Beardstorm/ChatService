1d9003c30201e2efab47afb7ae47e3b0b8eb0f49
Jonas Borggren <jonasborggren@gmail.com>
2013-12-11 14:03:23 +0100

    Updated GUI
    Jag har lagt till kommentarer och tagit bort skit som inte ananvändes.
    
    example.exampleSetting(); ligger nu före panel.add(example); så att det
    inte 'skapas' innan det är färdigkonfigurerat.

--- ChatClient/src/GUI.java
+++ ChatClient/src/GUI.java
@@ -9,7 +9,6 @@ import java.awt.event.KeyListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -29,29 +28,37 @@ public class GUI extends JFrame implements Observer
 		this.client = client;
 		client.setObserver(this);
 		
+		// Set window preferences
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setBounds(100, 100, 450, 260);
+		
+		// add content to the window
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		setContentPane(contentPane);
 
-		JPanel incomingPanel = new JPanel();
-		incomingPanel.setMaximumSize(new Dimension(450, 200));
-		incomingPanel.setPreferredSize(new Dimension(10, 200));
-		incomingPanel.setMinimumSize(new Dimension(10, 200));
-		contentPane.add(incomingPanel);
-		incomingPanel.setLayout(new BoxLayout(incomingPanel, BoxLayout.X_AXIS));
-		
+		// Set a panel holding panels
 		panel = new JPanel();
 		panel.setBorder(null);
 		panel.setOpaque(false);
 		panel.setPreferredSize(new Dimension(5, 200));
 		panel.setMinimumSize(new Dimension(5, 200));
 		panel.setMaximumSize(new Dimension(5, 200));
+		
+		// Set panel which holds the text area for messages
+		JPanel incomingPanel = new JPanel();
+		incomingPanel.setMaximumSize(new Dimension(450, 200));
+		incomingPanel.setPreferredSize(new Dimension(10, 200));
+		incomingPanel.setMinimumSize(new Dimension(10, 200));
+		incomingPanel.setLayout(new BoxLayout(incomingPanel, BoxLayout.X_AXIS));
+		contentPane.add(incomingPanel);
+	
+		// add the text-area-panel to parent panel. 
 		incomingPanel.add(panel);
 
+		// Create text-area
 		textArea = new TextArea();
 		textArea.setPreferredSize(new Dimension(200, 201));
 		textArea.setMaximumSize(new Dimension(2200, 200));
@@ -60,65 +67,65 @@ public class GUI extends JFrame implements Observer
 		textArea.setMinimumSize(new Dimension(200, 201));
 		incomingPanel.add(textArea);
 
+		// new panel for the message text-field + send button
 		inputPanel = new JPanel();
-
 		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
 
 		txtInputMessage = new JTextField();
 		txtInputMessage.setMaximumSize(new Dimension(2147483647, 30));
 		txtInputMessage.setToolTipText("Message");
 		txtInputMessage.setColumns(10);
-		
-		contentPane.add(inputPanel);
-		inputPanel.add(txtInputMessage);
-		// Buttons ahead
-		final JButton btnSend = new JButton("Send");
-		inputPanel.add(btnSend);
-
 		txtInputMessage.setEnabled(true);
 		txtInputMessage.setText("");
+		
+		// add
+		contentPane.add(inputPanel);
+		inputPanel.add(txtInputMessage);
+		
+		// The send button
+		final JButton btnSend = new JButton("Send");
 		btnSend.setEnabled(true);
 		
-		// Add actions to button
-		txtInputMessage.addKeyListener(
-		            new KeyListener(){
-		                public void keyPressed(KeyEvent e){
-		                    if(e.getKeyCode() == KeyEvent.VK_ENTER){
-		                		String message = txtInputMessage.getText().toString();
-		    					txtInputMessage.setText(""); 
-		    					sendMessage(message);
-		                    }       
-		                }
-						@Override
-						public void keyTyped(KeyEvent e) {}
-						@Override
-						public void keyReleased(KeyEvent e) {}
-		            }
-		        );
+		inputPanel.add(btnSend);
 		
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String input = txtInputMessage.getText().toString();
-				if (input == "" ) { 	
-					System.out.println("input: (" + input + ")");
-					JOptionPane.showMessageDialog(new JFrame(),
-							"Could not open file", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				} else {
+		// Add actions to text-field, making the Enter-button send messages.
+		txtInputMessage.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 					String message = txtInputMessage.getText().toString();
 					txtInputMessage.setText("");
 					sendMessage(message);
 				}
 			}
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+		});
+		
+		// Send-button action-listener
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+					// set "message" to the message actually typed into the text-field.
+					String message = txtInputMessage.getText().toString();
+					txtInputMessage.setText("");
+					
+					// send it!
+					sendMessage(message);
+			}
 		});
 	}
 	
+	// SendMessage method that wants a message, gets a message and sends that message.
 	public void sendMessage(String message)
 	{
 		client.send(message);
 	}
 	
-	@Override
+	// When a message is received by the server it shows in the text-area.
 	public void update(String message) 
 	{
 		textArea.append(message + System.lineSeparator());
