4987e0416e0373ae206d268785e22a77b8e90223
Mohanad Yousif <Samoyl3000@gmail.com>
2013-12-13 12:05:12 +0100

    updating the PropHandling
    I just remove unused method (writeProperties), the program don't need
    it.

--- ChatClient/src/PropHandling.java
+++ ChatClient/src/PropHandling.java
@@ -1,34 +1,17 @@
-
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropHandling {
-	
-	//Great a properties object
+
+	// Great a properties object
 	private Properties prop;
 
-	//Great a writing method for at save the port number and localhost
-	public void writerProperties(String fileName) {
-		prop = new Properties();
-		try {
-			//set the properties value
-			prop.setProperty("datorname", fileName);
-			prop.setProperty("port", fileName);
-			
-			//save properties to project root folder
-			prop.store(new FileOutputStream(fileName), null);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	//Great a reading method for at collect port number and localhost
+	// Great a reading method for at collect port number and localhost
 	public void readProperties(String fileName) {
 		prop = new Properties();
 		try {
-			//collect properties from project root folder
+			// collect properties from project root folder
 			prop.load(new FileInputStream(fileName));
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -36,8 +19,7 @@ public class PropHandling {
 	}
 
 	// Great String method for property
-	public String getProperty(String propName) 
-	{
+	public String getProperty(String propName) {
 		return prop.getProperty(propName);
 	}
 }
